68197e05344f4cc1a05c186540454111c723c613
Matthew Nolan <matthew.nolan@bjss.com>
2024-04-24 17:16:23 +0100

    add logic for text display in job cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,20 @@
-import React from 'react'
-import NavBar from './components/NavBar'
-import Hero from './components/Hero'
-import HomeCards from './components/HomeCards'
-import JobListings from './components/JobListings'
+import React from "react";
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs";
 
 const App = () => {
-  return (
-    <>
-    <NavBar />
-    <Hero />
-    <HomeCards />
-    <JobListings />
+    return (
+        <>
+            <NavBar />
+            <Hero />
+            <HomeCards />
+            <JobListings />
+            <ViewAllJobs />
+        </>
+    );
+};
 
-    <section className="m-auto max-w-lg my-10 px-6">
-      <a
-        href="jobs.html"
-        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >View All Jobs</a
-      >
-    </section>
-
-    </>
-  )
-}
-
-export default App
+export default App;
